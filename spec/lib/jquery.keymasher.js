3ea2219324fc85143fe4b8bcb72a7276532b7c2c
Josh Bush <josh@digitalbush.com>
2010-12-22 20:44:48 -0600

    Updating KeyMasher to the latest version

--- spec/lib/jquery.keymasher.js
+++ spec/lib/jquery.keymasher.js
@@ -6,13 +6,12 @@
 */
 
 (function($,undefined){
-	var modifiers={shift:16,ctrl:17,meta:224,alt:18};
 		//numberPad={'0':96,'1':97,'2':98,'3':99,'4':100,'5':101,'6':102,'7':103,'8':104,'9':105,'*':106,'+':107,'-':109,'.':110,'/':111},
 	
 	var keys=(function(){
 		var keys    = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`-=[]\\;',./ \t\n";
 		var shifted = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()~_+{}|:\"<>?";
-		var noprint={f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123,
+		var noprint={shift:16,ctrl:17,meta:91,alt:18,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123,
 					capslock:20,numlock:144,scrolllock:145,pageup:33,pagedown:34,end:35,home:36,backspace:8,
 					insert:45,del:46,pause:19,esc:27,left:37,up:38,right:39,down:40,printscreen:44};
 
@@ -23,29 +22,31 @@
 			if(shift)
 				defs[shift]={keyCode:keyCode,charCode:shift.charCodeAt(0),shift:value,requiresShift:index>=26};
 		});
-		$.each(noprint,function(key,value){defs[value]={keyCode:value};});
+		$.each(noprint,function(key,value){defs[key]={keyCode:value};});
 		return defs;
 	})();
 
 	var KeyMasher=function(elm){
 		var modifierState={alt: false, ctrl: false,	meta: false, shift: false};
-
-		var queueModifierEvent=function(direction,modifier){	
+		var forced={};
+		var queueModifierEvent=function(direction,modifier,isForced){
+			forced[modifier]=isForced;
 			modifierState[modifier]=(direction=='down');		
-			var event=$.extend($.Event(), modifierState, {type:'key'+direction, keyCode: modifiers[modifier], charCode: 0});
+			var event=$.extend($.Event(), modifierState, {type:'key'+direction, keyCode: keys[modifier].keyCode, charCode: 0});
 			elm.queue('keymash',function(){
 				elm.trigger(event);
 				$(this).dequeue('keymash');
 			});
-		};		
+		};
 		
 		var queueStroke=function(key){
 			if($.type(key)==='string')
 				key=keys[key];	
-			
-			if(modifierState.shift && key.shift)
+			if(key.requiresShift && !modifierState.shift)
+				queueModifierEvent('down','shift',true);
+			else if(modifierState.shift && key.shift)
 				key=keys[key.shift];
-
+			
 			var ignore = !key.charCode || modifierState.alt || modifierState.ctrl || modifierState.meta,
 				down = $.extend($.Event('keydown'), modifierState, {keyCode: key.keyCode, charCode: 0}),
 				press = $.extend($.Event('keypress'), modifierState, {keyCode: key.charCode, charCode: key.charCode}),
@@ -53,7 +54,7 @@
 			
 			elm.queue('keymash',function(){
 				elm.trigger(down);
-				if(!down.isPropagationStopped() || ignore){
+				if(!down.isPropagationStopped() && !ignore){
 					elm.trigger(press);
 					if(!press.isPropagationStopped()){					
 						//need to do caret positioning
@@ -63,6 +64,9 @@
 				elm.trigger(up);
 				$(this).dequeue('keymash');
 			});
+			
+			if(forced.shift)
+				queueModifierEvent('up','shift');			
 		};
 
 		var public={
@@ -92,8 +96,7 @@
 			fn=function(keys){keys.type(typing)};
 		}
 		return this.each(function(){
-			var elm=$(this);
-			fn(KeyMasher(elm));			
+			fn(KeyMasher($(this)));			
 		}).dequeue("keymash");
 	};
 })(jQuery);
