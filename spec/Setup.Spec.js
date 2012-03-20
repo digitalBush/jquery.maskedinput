5234a4c824433fe0afa6b0a8d66240883f69e85f
Fagner Martins Brack <github@fagnermartins.com>
2012-03-20 11:31:15 -0300

    Add the unit test simulating the issue #78 https://github.com/digitalBush/jquery.maskedinput/issues/78

--- spec/Setup.Spec.js
+++ spec/Setup.Spec.js
@@ -14,5 +14,19 @@ feature("Masking an Input", function() {
 			expect(input).toHaveValue('1_');
 		});
 	});
+	
+	scenario("Applying a mask to an input with a value", function() {
+		given('an input with a value', function() {
+			input.val('55');
+		});
+		
+		when("applying a mask with the template in both start and end", function() {
+			input.mask("(55)");
+		});
+		
+		then("value should be correct", function() {
+			expect(input).toHaveValue('(55)');
+		});
+	});
 });
 
