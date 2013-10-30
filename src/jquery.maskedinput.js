02ad44c2bf0d9a5638b7a876f88323123312d116
Jared Barboza <jared.m.barboza@gmail.com>
2013-10-29 21:21:05 -0400

    removes interpolation keys from banner comment

--- src/jquery.maskedinput.js
+++ src/jquery.maskedinput.js
@@ -1,8 +1,8 @@
 /*
 	Masked Input plugin for jQuery
-	Copyright (c) 2007-{{Year}} Josh Bush (digitalbush.com)
+	Copyright (c) 2007-2013 Josh Bush (digitalbush.com)
 	Licensed under the MIT license (http://digitalbush.com/projects/masked-input-plugin/#license)
-	Version: {{version}}
+	Version: 1.3
 */
 (function($) {
 
