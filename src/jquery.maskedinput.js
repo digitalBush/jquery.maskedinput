digitalBush/jquery.maskedinput

commit 23e6f693670bad821d9f56319e28886ffbeb5784

Patch for CSP Violation on FireFox OS

src/jquery.maskedinput.js
@@ -246,7 +246,13 @@ $.fn.extend({
 							next = seekNext(p);
 
 							if(android){
-								setTimeout($.proxy($.fn.caret,input,next),0);
+								//Path for CSP Violation on FireFox OS 1.1
+								var proxy = function()
+								{
+									$.proxy($.fn.caret,input,next);
+								}
+
+								setTimeout(proxy,0);
 							}else{
 								input.caret(next);
 							}

commit b6a0ebe9e99c5ccab47a5b66d58fd04b4c81a4ab

Fix caret position on Android
Fixes bug introduced in 23e6f693 - function created by $.proxy was never executed.

src/jquery.maskedinput.js
@@ -247,10 +247,9 @@ $.fn.extend({
 
 							if(android){
 								//Path for CSP Violation on FireFox OS 1.1
-								var proxy = function()
-								{
-									$.proxy($.fn.caret,input,next);
-								}
+								var proxy = function() {
+									$.proxy($.fn.caret,input,next)();
+								};
 
 								setTimeout(proxy,0);
 							}else{